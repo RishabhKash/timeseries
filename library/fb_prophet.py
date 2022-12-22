84911e12bbb501fc9f5f097d50ec111661ae6452
rishabhkashyap <rishabh.teresa@gmail.com>
2022-12-22 15:31:50 +0530

    autoregressive function created

---
+++ library/fb_prophet.py
@@ -0,0 +1,62 @@
+import logging
+import numpy as np
+import pandas as pd
+from prophet import Prophet
+from sklearn.metrics import mean_squared_error
+
+
+def ideal_prophet(data: pd.DataFrame, train_len: int, test_len: int, project: str):
+    """
+    Function to train Facebook's prophet algorithm
+    """
+
+    try:
+        data = data.reset_index()
+        data = data.rename(columns={project: "y", data.columns[0]: "ds"})
+        m = Prophet(daily_seasonality=False, weekly_seasonality=False)
+        model = m.fit(data[:train_len])
+        future = m.make_future_dataframe(periods=test_len, freq="M")
+        forecast = m.predict(future)
+
+        rmse = np.sqrt(mean_squared_error(data['y'][-test_len:], forecast['yhat'][-test_len:])).round(2)
+        mape = np.round(
+            np.mean(
+                np.abs(data['y'][-test_len:].values - forecast['yhat'][-test_len:].values)/
+                data['y'][-test_len:].values
+            ) * 100, 2
+        )
+
+        m1 = Prophet(daily_seasonality=False, weekly_seasonality=False)
+        fb_prophet = m1.fit(data)
+
+        return pd.DataFrame(
+            {
+                'Object': [fb_prophet],
+                'Method': ['fbprophet'],
+                'RMSE': [rmse],
+                'MAPE': [mape],
+                'p': [0],
+                'd': [0],
+                'q': [0],
+                'P': [0],
+                'D': [0],
+                'Q': [0]
+            }
+        )
+
+    except Exception as e:
+        logging.error("Error while training fb_prophet: %s", e)
+        return pd.DataFrame(
+            {
+                'Object': [None],
+                'Method': ['fbprophet'],
+                'RMSE': [np.inf],
+                'MAPE': [np.inf],
+                'p': [0],
+                'd': [0],
+                'q': [0],
+                'P': [0],
+                'D': [0],
+                'Q': [0]
+            }
+        )
---
+++ library/training_module.py
@@ -0,0 +1,172 @@
+import logging
+import numpy as np
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+from scipy.special import inv_boxcox
+from library.fb_prophet import ideal_prophet
+from library.holts_library import additive_trend, multiplicative_trend, a_a_seasonality, a_m_seasonality, m_a_seasonality, m_m_seasonality
+from library.arima_library import make_stationary, lagged_correlation, auto_regressive, arma, arima
+
+
+def train_predict(
+        processed_data: pd.DataFrame,
+        project: str
+):
+    """
+    Tests an array of time series algorithm and predicts for the next six months using the best performing model
+
+    Args:
+        processed_data: dataset
+        project: name of prediction column
+
+    Returns:
+        prediction for the next six months
+    """
+
+    data = processed_data.copy()
+    data.iloc[:, 0] = pd.to_datetime(data.iloc[:, 0])
+    data.iloc[:, 0] = data.iloc[:, 0].apply(lambda x: x + relativedelta(day=31))
+    start_date = datetime.now().strftime("%Y-%m")
+    end_date = (datetime.now() + relativedelta(months=5)).strftime("%Y-%m")
+    model_performance = pd.DataFrame()
+    data.loc[data[project] <= 0].interpolate(limit_direction="both")
+
+    # Deciding test length basis total available data
+    if (len(data) >= 3) and (len(date) <= 5):
+        test_len = 1
+    elif len(data) > 5:
+        test_len = 3
+    else:
+        return "Insufficient data for prediction"
+
+    train_len = len(data) - test_len
+    performance = pd.DataFrame()
+    logging.info(f" train length {train_len} test length {test_len}")
+
+    # training different time series models
+    outcome = ideal_prophet(data=data, train_len=train_len, test_len=test_len, project=project)
+    performance = pd.concat([performance, outcome])
+
+    outcome = additive_trend(data=data, train_len=train_len, test_len=test_len, project=project)
+    performance = pd.concat([performance, outcome])
+
+    outcome = multiplicative_trend(data=data, train_len=train_len, test_len=test_len, project=project)
+    performance = pd.concat([performance, outcome])
+
+    outcome = a_a_seasonality(data=data, train_len=train_len, test_len=test_len, project=project)
+    performance = pd.concat([performance, outcome])
+
+    outcome = a_m_seasonality(data=data, train_len=train_len, test_len=test_len, project=project)
+    performance = pd.concat([performance, outcome])
+
+    outcome = m_a_seasonality(data=data, train_len=train_len, test_len=test_len, project=project)
+    performance = pd.concat([performance, outcome])
+
+    outcome = m_m_seasonality(data=data, train_len=train_len, test_len=test_len, project=project)
+    performance = pd.concat([performance, outcome])
+
+    # data preparation for ARIMA family of algorithms
+    boxcox_data, lmbda, stationary_data, diff_order = make_stationary(data=data, project=project)
+    pacf, acf = lagged_correlation(boxcox_data=boxcox_data, differenced_data=stationary_data)
+
+    outcome = auto_regressive(
+        pacf=pacf,
+        boxcox_data=boxcox_data,
+        stationary_data=stationary_data,
+        test_len=test_len,
+        data=data,
+        diff_order=diff_order,
+        lmbda=lmbda,
+        project=project
+    )
+    performance = pd.concat([performance, outcome])
+
+    outcome = arma(
+        pacf=pacf,
+        acf=acf,
+        boxcox_data=boxcox_data,
+        stationary_data=stationary_data,
+        test_len=test_len,
+        data=data,
+        diff_order=diff_order,
+        lmbda=lmbda,
+        project=project
+    )
+    performance = pd.concat([performance, outcome])
+
+    outcome = arima(
+        boxcox_data=boxcox_data,
+        train_len=train_len,
+        data=data,
+        lmbda=lmbda,
+        project=project
+    )
+    if ((outcome['p']) | (outcome['P'])).any():
+        performance = pd.concat([performance, outcome])
+
+    performance = performance.sort_values(by=['RMSE', 'MAPE'])
+    model_performance = pd.concat([model_performance, performance])
+
+    # Prediction for next six months
+    if performance.iloc[0][1] in ('simple_exponential_smoothing', 'exponential_smoothing_add',
+                                  'exponential_smoothing_mul', 'exponential_smoothing_seasonality_a_a',
+                                  'exponential_smoothing_seasonality_a_m', 'exponential_smoothing_seasonality_m_a',
+                                  'exponential_smoothing_seasonality_m_m'):
+        result = performance.iloc[0][0].predict(start_date, end_date)
+        result = pd.DataFrame(result)
+        result.columns = ['Predicted']
+
+    elif performance.iloc[0][1] in ('auto_regressive', 'moving_average', 'ar_ma'):
+        if performance.iloc[0][5] > 0:
+            result = performance.iloc[0][0].predict(stationary_data.index.min(), end_date)
+            result = pd.DataFrame(result)
+            result.columns = ['Predicted']
+            result['Predicted'] = result['Predicted'].cumsum()
+            count = 0
+            while count < performance.iloc[0][5]:
+                result['Predicted'] = result['Predicted'].add(boxcox_data[count])
+                count += 1
+
+        else:
+            result = performance.iloc[0][0].predict(boxcox_data.index.min(), end_date)
+            result = pd.DataFrame(result)
+            result.columns = ['Predicted']
+
+        result['Predicted'] = inv_boxcox(result['Predicted'], lmbda)
+        result = result[-6:]
+
+    elif performance[0][1] in ('fbprophet'):
+        temp = performance.iloc[0][0].make_future_dataframe(periods=6, freq="M")
+        future = performance.iloc[0][0].predict(temp)
+        result = future[["ds", "yhat"]][-6:]
+        result.set_index("ds", inplace=True)
+
+    else:
+        result = performance.iloc[0][0].predict(6)
+        result = pd.DataFrame(result)
+        result.columns = ['Predicted']
+        result['Predicted'] = inv_boxcox(result['Predicted'], lmbda)
+
+    result = round(result, 0).abs()
+
+    return model_performance, result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
